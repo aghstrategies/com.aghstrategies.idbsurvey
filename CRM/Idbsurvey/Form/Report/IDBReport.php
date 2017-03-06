09d76cc83e64f8f04354f0dd173e036a4595120c
Andrew Hunt <andrew@aghstrategies.com>
2017-03-06 15:18:21 -0500

    Changes for 2016 data

--- CRM/Idbsurvey/Form/Report/IDBReport.php
+++ CRM/Idbsurvey/Form/Report/IDBReport.php
@@ -63,11 +63,11 @@ class CRM_Idbsurvey_Form_Report_IDBReport extends CRM_Report_Form {
                   ON og.name = 'contribution_status'
                     AND og.id = ov.option_group_id
                 LEFT JOIN civicrm_financial_type ft ON contribution.financial_type_id = ft.id";
-    $where = " WHERE ( contribution.receive_date >= 20150101000000 )
-                 AND ( contribution.receive_date < 20160101000000 )
+    $where = " WHERE ( contribution.receive_date >= 20160101000000 )
+                 AND ( contribution.receive_date < 20170101000000 )
                  AND cc.is_deleted = 0";
-    $where2014 = " WHERE ( contribution.receive_date >= 20140101000000 )
-                     AND ( contribution.receive_date < 20150101000000 )
+    $where2015 = " WHERE ( contribution.receive_date >= 20150101000000 )
+                     AND ( contribution.receive_date < 20160101000000 )
                      AND cc.is_deleted = 0";
 
     // Most questions just looking at donations:
@@ -77,17 +77,17 @@ class CRM_Idbsurvey_Form_Report_IDBReport extends CRM_Report_Form {
     $incExc = (CRM_Utils_Array::value('financial_type_id_op', $this->_submitValues) == 'mnot') ? 'NOT IN' : 'IN';
     $typesClause = empty($types) ? 'IS NOT NULL' : "$incExc ({$types})";
     $whereDon = "$where AND contribution.financial_type_id $typesClause";
-    $whereDon2014 = "$where2014 AND contribution.financial_type_id $typesClause";
+    $whereDon2015 = "$where2015 AND contribution.financial_type_id $typesClause";
 
     // Individual donors (for the purposes of this, households are individuals):
     $whereInd = "$whereDon AND ( cc.contact_type in ('Individual', 'Household') )";
-    $whereInd2014 = "$whereDon2014 AND ( cc.contact_type in ('Individual', 'Household') )";
+    $whereInd2015 = "$whereDon2015 AND ( cc.contact_type in ('Individual', 'Household') )";
 
     // Online donations by individuals:
     $whereOnlineInd = "$whereInd
       AND contribution.contribution_page_id IS NOT NULL
       AND contribution.contribution_recur_id IS NULL";
-    $whereOnlineInd2014 = "$whereInd2014
+    $whereOnlineInd2015 = "$whereInd2015
       AND contribution.contribution_page_id IS NOT NULL
       AND contribution.contribution_recur_id IS NULL";
 
@@ -104,24 +104,24 @@ class CRM_Idbsurvey_Form_Report_IDBReport extends CRM_Report_Form {
 
     /* Specific questions */
 
-    // 1. Total income/revenue for 2015
-    $questions[1] = self::tsLocal("What was your organization's total income/revenue in 2015?");
+    // 1. Total income/revenue for 2016
+    $questions[1] = self::tsLocal("What was your organization’s total revenue?");
     $this->runItemQuery($select . $from . $where, 1);
 
-    // 3. Total raised by individuals for 2015
+    // 3. Total raised by individuals for 2016
     // 4. Total individual donors
-    $questions[3] = self::tsLocal('What was the total amount raised from individuals in 2015?');
-    $questions[4] = self::tsLocal('How many individuals donated in 2015?');
-    $this->runItemQuery($selectContacts . $from . $whereInd, 3, 4);
+    $questions[2] = self::tsLocal('What was the total amount raised from individuals? Please include online and offline donations from direct mail, email, major donors, and other individual donor strategies.');
+    $questions[3] = self::tsLocal('How many individuals donated?');
+    $this->runItemQuery($selectContacts . $from . $whereInd, 2, 3);
 
     // 5. What is your retention rate?
-    $questions[5] = self::tsLocal('What is your retention rate?');
+    $questions[4] = self::tsLocal('What is your retention rate? Retention rate is the percentage of people who gave last year that gave again this year.');
     $selectRetained = "$selectContacts, IF(og2.id IS NULL,0,1) as retained";
     $retainedFrom = "$from
       LEFT JOIN civicrm_contribution c2
         ON c2.contact_id = contribution.contact_id
-        AND c2.receive_date >= 20150101000000
-        AND c2.receive_date < 20160101000000
+        AND c2.receive_date >= 20160101000000
+        AND c2.receive_date < 20170101000000
         AND c2.financial_type_id $typesClause
       LEFT JOIN civicrm_option_value ov2
         ON ov2.name = 'Completed'
@@ -132,50 +132,48 @@ class CRM_Idbsurvey_Form_Report_IDBReport extends CRM_Report_Form {
       LEFT JOIN civicrm_financial_type ft2
         ON c2.financial_type_id = ft2.id";
 
-    $dao = CRM_Core_DAO::executeQuery($selectRetained . $retainedFrom . $whereInd2014 . ' GROUP BY retained');
+    $dao = CRM_Core_DAO::executeQuery($selectRetained . $retainedFrom . $whereInd2015 . ' GROUP BY retained');
     $results = array();
     while ($dao->fetch()) {
       $results[$dao->retained] = $dao->total_contacts;
     }
     if (empty($results[1])) {
-      $this->answers[5] = '0%';
+      $this->answers[4] = '0%';
     }
     elseif (empty($results[0])) {
-      $this->answers[5] = '100%';
+      $this->answers[4] = '100%';
     }
     else {
       $pct = 100 * $results[1] / ($results[0] + $results[1]);
       $pct = round($pct, 2);
-      $this->answers[5] = "$pct%";
+      $this->answers[4] = "$pct%";
     }
 
     // 6. Total raised online from individuals
     // 7. Number of people that gave online
-    $questions[6] = self::tsLocal('How much did you raise online from individuals in 2015?');
-    $questions[7] = self::tsLocal('How many individuals gave online in 2015?');
-    $this->runItemQuery($selectContacts . $from . $whereOnlineInd, 6, 7);
+    $questions[5] = self::tsLocal('How much did you raise online from individuals? Please do not include recurring (monthly, quarterly, etc.) in this total.');
+    $questions[6] = self::tsLocal('How many individuals gave online?');
+    $this->runItemQuery($selectContacts . $from . $whereOnlineInd, 5, 6);
 
-    // 8. How much was given in total through recurring (monthly, quarterly, etc) donations in 2015?
-    // 9. How many individuals made recurring donations in 2015?
-    $questions[8] = self::tsLocal('How much was given in total through recurring (monthly, quarterly, etc) donations in 2015?');
-    $questions[9] = self::tsLocal('How many individuals made recurring donations in 2015?');
-    $this->runItemQuery($selectContacts . $from . $whereRec, 8, 9);
+    // 8. How much was given in total through recurring (monthly, quarterly, etc) donations in 2016?
+    // 9. How many individuals made recurring donations in 2016?
+    $questions[7] = self::tsLocal('How much was given in total through recurring (monthly, quarterly, etc) donations?');
+    $questions[8] = self::tsLocal('How many individuals made recurring donations?');
+    $this->runItemQuery($selectContacts . $from . $whereRec, 7, 8);
 
-    // 10. How much did you raise from individuals giving $1,000 or more (in total) in 2015?
-    // 11. How many people made gifts of $1,000 or more (in total) in 2015?
-    $questions[10] = self::tsLocal('How much did you raise from individuals giving $1,000 or more (in total) in 2015?');
-    $questions[11] = self::tsLocal('How many individuals made gifts of $1,000 or more (in total) in 2015?');
+    // 10. How much did you raise from individuals giving $1,000 or more (in total) in 2016?
+    $questions[9] = self::tsLocal('How much did you raise from individuals giving $1,000 or more?');
     $groupBy1K = " GROUP BY contribution.contact_id
                    HAVING total_amount_sum >= 1000";
     $query1K = "SELECT SUM(total_amount_sum) as total_amount_sum, COUNT(DISTINCT contact_id) as total_contacts
                 FROM ($select, contribution.contact_id as contact_id $from $whereInd $groupBy1K) bigdonors";
-    $this->runItemQuery($query1K, 10, 11);
+    $this->runItemQuery($query1K, 9);
 
     // 12. Check if memberships are enabled and active
-    $questions[12] = self::tsLocal('Does your organization offer memberships?');
+    $questions[10] = self::tsLocal('Does your organization offer memberships?');
     $components = CRM_Core_Component::getEnabledComponents();
     if (empty($components['CiviMember'])) {
-      $this->answers[12] = self::tsLocal('CiviMember is disabled');
+      $this->answers[10] = self::tsLocal('CiviMember is disabled');
     }
     else {
       try {
@@ -184,28 +182,16 @@ class CRM_Idbsurvey_Form_Report_IDBReport extends CRM_Report_Form {
           'context' => "search",
         ));
         if (empty($memberships['values'])) {
-          $this->answers[12] = self::tsLocal('No memberships');
+          $this->answers[10] = self::tsLocal('No memberships');
         }
         else {
-          $this->answers[12] = self::tsLocal('Yes:') . ' <ul><li>' . implode('</li><li>', $memberships['values']) . '</li></ul>';
+          $this->answers[10] = self::tsLocal('Yes:') . ' <ul><li>' . implode('</li><li>', $memberships['values']) . '</li></ul>';
         }
       }
       catch (CiviCRM_API3_Exception $e) {
-        $this->answers[12] = 'API Error: ' . $e->getMessage();
+        $this->answers[10] = 'API Error: ' . $e->getMessage();
       }
     }
-
-    // 14. What was your organization's total income/revenue in 2014?
-    $questions[14] = self::tsLocal("What was your organization's total income/revenue in 2014?");
-    $this->runItemQuery($select . $from . $where2014, 14);
-
-    //15.  What was your organization's total income from individual donors in 2015?
-    $questions[15] = self::tsLocal("What was your organization's total income from individual donors in 2014?");
-    $this->runItemQuery($select . $from . $whereInd2014, 15);
-
-    //16.  What was your organization's total income from online donations in 2013?
-    $questions[16] = self::tsLocal("What was your organization's total income from online donations in 2014?");
-    $this->runItemQuery($select . $from . $whereOnlineInd2014, 16);
 
     $this->assign('questions', $questions);
     $this->assign('answers', $this->answers);
